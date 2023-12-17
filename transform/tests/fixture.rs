34931177b622afb13529476db9a44d912a54564c
leegeunhyeok <dev.ghlee@gmail.com>
2023-12-17 13:17:21 +0900

    test: refactor fixtures

--- transform/tests/fixture.rs
+++ transform/tests/fixture.rs
@@ -4,25 +4,8 @@ use swc_core::ecma::parser::{Syntax, TsConfig};
 use swc_ecma_transforms_testing::test_fixture;
 use swc_nativewind::nativewind;
 
-#[testing::fixture("tests/fixture/default/**/input.js")]
-fn default_fixture(input: PathBuf) {
-    let filename = input.to_string_lossy();
-    let output = input.with_file_name("output.js");
-
-    test_fixture(
-        Syntax::Typescript(TsConfig {
-            tsx: filename.ends_with(".tsx"),
-            ..Default::default()
-        }),
-        &|_| nativewind(String::from("/someFile.js")),
-        &input,
-        &output,
-        Default::default(),
-    );
-}
-
-#[testing::fixture("tests/fixture/denied_file/**/input.js")]
-fn denied_file_fixture(input: PathBuf) {
+#[testing::fixture("tests/fixture/**/input.js")]
+fn fixture(input: PathBuf) {
     let filename = input.to_string_lossy();
     let output = input.with_file_name("output.js");
 
@@ -32,9 +15,13 @@ fn denied_file_fixture(input: PathBuf) {
             ..Default::default()
         }),
         &|_| {
-            nativewind(String::from(
-                "/node_modules/react-native-css-interop/someFile.js",
-            ))
+            nativewind(String::from(if filename.contains("denied_modules") {
+                // denied file name
+                "/node_modules/react-native-css-interop/someFile.js"
+            } else {
+                // allowed file name
+                "/someFile.js"
+            }))
         },
         &input,
         &output,
